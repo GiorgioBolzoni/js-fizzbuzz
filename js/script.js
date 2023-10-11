bd2edcd0eff53e89ad27ca2b6aaec8bf5768243d
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-11 18:17:46 +0200

    added some css animations and copied js outputs on console

--- js/script.js
+++ js/script.js
@@ -13,20 +13,24 @@ for(let i = 1; i <= 100; i++){
         boxEl.className = 'fizzBuzz';
         wrapperEl.append (boxEl)
         boxEl.innerHTML = fizzBuzz
+        console.log('fizzBuzz')
     }else if(i % 5 === 0){
         boxEl.className = 'buzz';
         // boxEl.append (wrapperEl)
         wrapperEl.append (boxEl)
         boxEl.innerHTML = buzz
+        console.log('buzz')
     }else if(i % 3 === 0){
         boxEl.className = 'fizz';
         // boxEl.append (wrapperEl)
         wrapperEl.append (boxEl)
         boxEl.innerHTML = fizz
+        console.log('fizz')
     }else {
         boxEl.className = 'box';
         // boxEl.append (wrapperEl)
         wrapperEl.append (boxEl)
         boxEl.innerHTML = i
+        console.log(i)
     }
 };
