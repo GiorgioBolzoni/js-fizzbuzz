35bc03c251db1bca998d17b35b6f2ddae271d7a0
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-11 17:07:22 +0200

    completed js rules and fixed layout on css

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const wrapperEl = document.getElementById('boxWrapper');
+
+const fizz = 'fizz';
+const buzz = 'buzz';
+const fizzBuzz = 'fizzBuzz';
+
+
+for(let i = 1; i <= 100; i++){
+    const boxEl = document.createElement('div')
+    //istruzioni da ripetere
+    // console.log(i);
+    if(i % 3 === 0 && i % 5 === 0){
+        boxEl.className = 'fizzBuzz';
+        wrapperEl.append (boxEl)
+        boxEl.innerHTML = fizzBuzz
+    }else if(i % 5 === 0){
+        boxEl.className = 'buzz';
+        // boxEl.append (wrapperEl)
+        wrapperEl.append (boxEl)
+        boxEl.innerHTML = buzz
+    }else if(i % 3 === 0){
+        boxEl.className = 'fizz';
+        // boxEl.append (wrapperEl)
+        wrapperEl.append (boxEl)
+        boxEl.innerHTML = fizz
+    }else {
+        boxEl.className = 'box';
+        // boxEl.append (wrapperEl)
+        wrapperEl.append (boxEl)
+        boxEl.innerHTML = i
+    }
+};
